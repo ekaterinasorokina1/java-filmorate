103bf9857d80b4b36cb5cac3471f7027f85c552a
Ekaterina Sorokina <ekaterina.sorokina@ural.rt.ru>
2025-09-11 02:24:12 +0500

    fix(add-database): стили код

--- src/main/java/ru/yandex/practicum/filmorate/dto/UpdateFilmRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/UpdateFilmRequest.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.dto;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import ru.yandex.practicum.filmorate.annotation.MinReleaseDate;

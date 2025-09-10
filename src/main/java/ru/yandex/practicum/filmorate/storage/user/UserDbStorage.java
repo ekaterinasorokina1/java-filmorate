6db213d3392b17eb8abcb90958290dac6d46203f
Ekaterina Sorokina <ekaterina.sorokina@ural.rt.ru>
2025-09-11 02:30:36 +0500

    fix(add-database): стили код

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -18,17 +18,15 @@ public class UserDbStorage extends BaseRepository<User> implements UserStorage {
     private static final String UPDATE_QUERY = "UPDATE users SET email = ?, login = ?, name = ? WHERE user_id = ?";
     private static final String ADD_FRIEND_QUERY = "INSERT INTO friends (user_id, friend_id) " +
             "VALUES (?, ?)";
-    final String FRIEND_GET_QUERY =
-            "SELECT * FROM users u " +
-                    "JOIN friends f " +
-                    "ON u.user_id = f.friend_id " +
-                    "WHERE f.user_id = ?";
+    final String FRIEND_GET_QUERY = "SELECT * FROM users u " +
+            "JOIN friends f " +
+            "ON u.user_id = f.friend_id " +
+            "WHERE f.user_id = ?";
     final String REMOVE_FRIEND_QUERY = "DELETE FROM friends WHERE user_id = ? AND friend_id = ?";
-    final String GET_COMMON_FRIENDS_QUERY =
-            "SELECT * FROM users u " +
-                    "JOIN friends f1 ON u.user_id = f1.friend_id " +
-                    "JOIN friends f2 ON u.user_id = f2.friend_id " +
-                    "WHERE f1.user_id = ? AND f2.user_id = ?";
+    final String GET_COMMON_FRIENDS_QUERY = "SELECT * FROM users u " +
+            "JOIN friends f1 ON u.user_id = f1.friend_id " +
+            "JOIN friends f2 ON u.user_id = f2.friend_id " +
+            "WHERE f1.user_id = ? AND f2.user_id = ?";
 
 
     public UserDbStorage(JdbcTemplate jdbc, RowMapper<User> mapper) {

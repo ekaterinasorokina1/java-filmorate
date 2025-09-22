c91d4cdede55c8ae381a5f619e632ca97123984a
Artur Ryabov <B150B@yandex.ru>
2025-09-22 14:34:20 +0500

    Исправил стилистические ошибки

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -75,7 +75,7 @@ public class UserController {
     }
 
 
-   @GetMapping("/{id}/feed")
+    @GetMapping("/{id}/feed")
     @ResponseStatus(HttpStatus.OK)
     public List<Feed> getFeed(@PathVariable int id) {
         return userService.getFeed(id);

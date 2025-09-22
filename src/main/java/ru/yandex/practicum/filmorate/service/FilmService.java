8b01644b9cec052d929532b2a3c051ab290afb2a
Elena Nikolaeva <elenanikolaevaya@yandex.ru>
2025-09-22 12:31:34 +0500

    Добавление режиссеров v.8 решение конфликтов (спринт 13)

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,8 +2,8 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Director;
+import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.FeedEventType;
